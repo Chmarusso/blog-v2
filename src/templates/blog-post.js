165e6a82bceb06bd9c635dc598b22e4c143dd869
Artur Ch <chmarus@gmail.com>
2020-02-08 13:06:15 +0100

    Update newsletter

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -24,21 +24,27 @@ export default ({ data }) => {
         <h1 className="f2">{post.frontmatter.title}</h1>
         <div className="lh-copy mt3" dangerouslySetInnerHTML={{ __html: post.html }} />
         <div className="pa4-l">
-        <form class="newsletterBox mw7 center pa4 br2-ns ba b--black-10" action="https://gmail.us20.list-manage.com/subscribe/post?u=cd9534d13c2fcd4e20e32977c&amp;id=55b16eeb45" method="post" name="mc-embedded-subscribe-form" target="_blank">
-    <fieldset className="cf bn ma0 pa0">
-      <legend className="pa0 f5 f4-ns mb3 b">Nie przegap nowych materiałów i zapisz się na Newsletter!</legend>
-      <div className="cf">
-        <label className="clip" for="email-address">Email Address</label>
-        <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 w-100 w-75-m w-80-l br2-ns br--left-ns" name="EMAIL" id="mce-EMAIL" placeholder="Twój adres e-mail" required />
-        <div style={{position: 'absolute', left: '-5000px'}}><input type="text" name="b_cd9534d13c2fcd4e20e32977c_55b16eeb45" tabindex="-1" value="" /></div>
-        <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Subskrybuj" />
+          <form class="newsletterBox mw7 center pa4 br2-ns ba b--black-10" action="https://gmail.us20.list-manage.com/subscribe/post?u=cd9534d13c2fcd4e20e32977c&amp;id=55b16eeb45" method="post" name="mc-embedded-subscribe-form" target="_blank">
+          <fieldset className="cf bn ma0 pa0">
+            <legend className="pa0 f5 f4-ns mb3 b">Zapisz się na Newsletter!</legend>
+            ✅ Zero spamu
+            <br/>✅ Tylko dobre materiały (React, JS, Web UX/UI)
+            <br/>✅ W każdej chwili możesz się wypisać
+            <br/><br/>
+            <div className="cf">
+              <label className="clip" for="email-address">Email Address</label>
+              <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 w-100 w-50-m w-30-l br2-ns br--left-ns" name="FNAME" id="mce-FNAME" placeholder="Twoje imię" />
+              <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 w-100 w-50-m w-50-l br2-ns br--right-ns" name="EMAIL" id="mce-EMAIL" placeholder="Twój adres e-mail" required />
+              <div style={{position: 'absolute', left: '-5000px'}}><input type="text" name="b_cd9534d13c2fcd4e20e32977c_55b16eeb45" tabindex="-1" value="" /></div>
+              <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Subskrybuj" />
+            </div>
+          </fieldset>
+          <div>
+            <br/>
+            Sprawdź informacje o polityce prywatności i RODO <a href="/privacy-policy-cookies">tutaj</a>.
+          </div>
+        </form>
       </div>
-    </fieldset>
-    <div><br/>Z newslettera w każdej chwili można się bardzo łatwo wypisać. Wystarczy w stopce wiadomości kliknąć w link i już nigdy nie otrzymasz kolejnej wiadomości. Jeżeli interesuje Cię polityka prywatności oraz sprawy związane z tzw. RODO to zapraszam pod <a href="/privacy-policy-cookies">ten link</a>.
-</div>
-  </form>
-
-</div>
 
         <div>
           <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
